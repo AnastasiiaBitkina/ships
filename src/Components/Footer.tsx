364d2a48a82246e454bbfa92f5ea7a0f0a59baa3
anastasia <anastasia.ethno@gmail.com>
2023-11-01 19:45:24 +0100

    feat: add Footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <h3>Anastasiia Bitkina</h3>
+            <p>@ethno_genesis</p>
+        </div>
+    )
+}
+export default Footer;
